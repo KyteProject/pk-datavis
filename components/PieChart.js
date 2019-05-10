c429acd409d8a8e529897c8c08a8d8a9a452060a
Dan <headhertz88@gmail.com>
2019-05-10 10:42:12 +0100

    pie with svg  elements

--- components/PieChart.js
+++ components/PieChart.js
@@ -1,54 +1,41 @@
 import React, { useEffect, useRef } from 'react';
 import * as d3 from 'd3';
 
+const Arc = ( { data, index, createArc, colors, format } ) => (
+	<g key={index} className='arc'>
+		<path className='arc' d={createArc( data )} fill={colors( index )} />
+		<text
+			transform={`translate(${createArc.centroid( data )})`}
+			textAnchor='middle'
+			alignmentBaseline='middle'
+			fill='white'
+			fontSize='10'
+		>
+			{format( data.value )}
+		</text>
+	</g>
+);
+
 const Pie = ( props ) => {
-	const ref = useRef( null ),
+	const createPie = d3
+		.pie()
+		.value( ( d ) => d.value )
+		.sort( null );
+	const createArc = d3
+		.arc()
+		.innerRadius( props.innerRadius )
+		.outerRadius( props.outerRadius );
+	const colors = d3.scaleOrdinal( d3.schemeCategory10 ),
 		format = d3.format( '.2f' ),
-		colours = d3.scaleOrdinal( d3.schemeCategory10 ),
-		createPie = d3
-			.pie()
-			.value( ( d ) => d.value )
-			.sort( null ),
-		createArc = d3
-			.arc()
-			.innerRadius( props.innerRadius )
-			.outerRadius( props.outerRadius );
-
-	useEffect(
-		() => {
-			const data = createPie( props.data ),
-				group = d3.select( ref.current ),
-				groupWithData = group.selectAll( 'g.arc' ).data( data );
-
-			groupWithData.exit().remove();
-
-			const groupWithUpdate = groupWithData
-				.enter()
-				.append( 'g' )
-				.attr( 'class', 'arc' );
-
-			const path = groupWithUpdate.append( 'path' ).merge( groupWithData.select( 'path.arc' ) ),
-				text = groupWithUpdate.append( 'text' ).merge( groupWithData.select( 'text' ) );
-
-			path
-				.attr( 'class', 'arc' )
-				.attr( 'd', createArc )
-				.attr( 'fill', ( d, i ) => colours( i ) );
-
-			text
-				.attr( 'text-anchor', 'middle' )
-				.attr( 'alignment-baseline', 'middle' )
-				.attr( 'transform', ( d ) => `translate(${createArc.centroid( d )})` )
-				.style( 'fill', 'white' )
-				.style( 'font-size', 10 )
-				.text( ( d ) => format( d.value ) );
-		},
-		[ colours, createArc, createPie, format, props.data ]
-	);
+		data = createPie( props.data );
 
 	return (
 		<svg width={props.width} height={props.height}>
-			<g ref={ref} transform={`translate(${props.outerRadius} ${props.outerRadius})`} />
+			<g transform={`translate(${props.outerRadius} ${props.outerRadius})`}>
+				{data.map( ( d, i ) => (
+					<Arc key={i} data={d} index={i} createArc={createArc} colors={colors} format={format} />
+				) )}
+			</g>
 		</svg>
 	);
 };
